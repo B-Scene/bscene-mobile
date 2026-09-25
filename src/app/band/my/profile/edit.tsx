e5b0c386dc61f327a4c963e3afd4f1cbcf60689d
김재범 <process68@naver.com>
2026-09-25 16:18:02 +0900

    [Feat] 밴드 프로필 수정 라우트 추가

---
+++ src/app/band/my/profile/edit.tsx
@@ -0,0 +1,9 @@
+import {
+    BandProfileEditScreen,
+} from "@/features/band/BandProfileEditScreen";
+
+export default function BandProfileEditRoute() {
+  return (
+    <BandProfileEditScreen />
+  );
+}
