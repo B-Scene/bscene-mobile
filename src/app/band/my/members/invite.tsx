ac513f261c92dfd06d3ee23d3c09866011c6d4a2
김재범 <process68@naver.com>
2026-09-25 16:20:38 +0900

    [Feat] 밴드 멤버 초대 라우트 추가

---
+++ src/app/band/my/members/invite.tsx
@@ -0,0 +1,9 @@
+import {
+    BandMemberInviteScreen,
+} from "@/features/band/BandMemberInviteScreen";
+
+export default function BandMemberInviteRoute() {
+  return (
+    <BandMemberInviteScreen />
+  );
+}
